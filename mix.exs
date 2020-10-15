7fbffa6084f96ce6446883892e6d2a0d521cd10a
Jon Lunsford <jon@capturethecastle.net>
2020-10-15 11:33:16 -0700

    enable os_mon

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule MycoBotUi.MixProject do
   def application do
     [
       mod: {MycoBotUi.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
